9e1eb7a8faadf0e8e88bf14fe02ef608a5d8c4e3
sherzod89 <sherzodms@inbox.ru>
2022-03-06 18:20:49 +0300

    02

--- sum_even.py
+++ sum_even.py
@@ -12,4 +12,3 @@ number4=var_int//10
 sum_even=0
 #Find the sum of the even digits in the variable "var_int".
 sum_even_digits=number1-(number1*(number1%2))+number2-(number2*(number2%2))+number3-(number3*(number3%2))+number4-(number4*(number4%2))
-print(sum_even_digits)
